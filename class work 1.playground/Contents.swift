f4764dc6491e9f16d14fd90b4fd09c2cc36ae1c0
alhamad478 <70263707+alhamad478@users.noreply.github.com>
2020-09-09 20:24:29 +0300

    done

---
+++ class work 1.playground/Contents.swift
@@ -0,0 +1,18 @@
+let name = "abdullah"
+var hobby1 = "soccer"
+var hobby2 = "gaming"
+var age = 16
+var height = 165
+print("My name is \(name) I am \(age), I like \(hobby1 + " "  + hobby2)")
+
+let friendName = "ali"
+var friendHobby1 = "soccer"
+var friendHobby2 = "drawing"
+var freindAge = 14
+var friendHeight = 170
+print(" my friend name is \(friendName) he is \(freindAge), he likes \(friendHobby1 + " "  + friendHobby2)")
+var ageDifference = age - freindAge
+
+var mutualHobbies = hobby1 == hobby2 || friendHobby1 == friendHobby2 || hobby1 == friendHobby1 || hobby2 == friendHobby2 || hobby1 == friendHobby2 || hobby2 == friendHobby1
+
+
